7c038df32c10f023ab2a7ba4fec98f40c891887e
toandzpro <kimthoai15620@gmail.com>
2024-05-25 00:57:56 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class baitap27
+{
+    public static void Main()
+    {
+        Student.Change();//calling change method
+                         //creating objects
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+        //calling display method
+        s1.Display();
+        s2.Display();
+        s3.Display();
+    }
+}
+public class Student
+{
+    private int rollno;
+    private string name;
+    private static string college = "BBDIT";
+    public Student(int r, string n)
+    {
+        rollno = r;
+        name = n;
+    }
+    public static void Change()
+    {
+        college = "CODEGYM";
+    }
+    public void Display()
+    {
+        Console.WriteLine(rollno + " " + name + " " + college);
+    }
+}
+
+
+
+
